30e49de8a25e17c3a5a19a5b9b05bc7aeae21023
DimaEvseenko <youdima70@gmail.com>
2023-09-17 15:45:31 +0300

    course

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-
-        Player.say("Hello");
+        printMegaBytesAndKiloBytes(0);
     }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes < 0)
+            System.out.println("Invalid Value");
+        else{
+            int megaBytes = kiloBytes / 1024;
+            int remKiloBytes = (megaBytes==0)?0:kiloBytes % (megaBytes * 1024);
+            System.out.println(kiloBytes+" KB = "+megaBytes+" MB and "+remKiloBytes+" KB");
+        }
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay < 0 || hourOfDay > 23)
+            return false;
+        return barking && (hourOfDay < 8 || hourOfDay > 22);
+    }
+
 }
